1a7f4f4e93fd65373e8a27548cd3802d6d3bec1c
k.sawada <63055099+koukisawada1@users.noreply.github.com>
2022-03-13 01:06:23 +0900

    メインページ作成

--- all.js
+++ all.js
@@ -1,24 +1,81 @@
-//スライドショー
-window.onload = function() {
-	var mySwiper = new Swiper ('.swiper-container', {
-		slidesPerView:1.5,//画像を何枚表示するか
-		spaceBetween: 10,//何ピクセル画像の間隔をあけるか
-		centeredSlides : true,//見切らせたい場合メイン画像をセンターにもってくるか
-		//自動再生する場合
-		autoplay: {
-		delay: 3000, //3秒後に次の画像に代わる
-		},
-		loop: true,//最後の画像までいったらループする
-		//ページネーションをつける場合
-		pagination: {
-		 el: '.swiper-pagination',
-		 type: 'bullets',
-		 clickable: true,
-		},
-		//左右のナビゲーションをつける場合
-		navigation: {
-		 nextEl: '.swiper-button-next',
-		 prevEl: '.swiper-button-prev',
-		}
-	});
-}
+$(function() {
+
+
+    //オープニングアニメーション
+    setTimeout(function(){
+        $('#opening').find('span').fadeOut(1000);
+    },900); //0.5秒後にロゴをフェードイン!
+    setTimeout(function(){
+		$('#opening').fadeOut(1300);
+	},1800); //0.5秒後にロゴをフェードイン!
+
+    //サイドバー色制御
+    $('#top-contents').scroll(function() {
+        var scroll = $(this).scrollTop();
+        var home = $('#home').position().top;
+        var works = $('#works').position().top;
+        var profile = $('#profile').position().top;
+        var skill = $('#skill').position().top;
+        var contact = $('#contact').position().top;
+        var a = $('#contact').offset();
+        //console.log(a);
+        
+        if(home == 0) {
+            $('#sidebar').find('li').find('a').removeClass('active');
+            $('#home_link').addClass('active');
+        } else if(works < 100 && works > -100) {
+            $('#sidebar').find('li').find('a').removeClass('active');
+            $('#works_link').addClass('active');
+        } else if (profile < 100 && profile > -100) {
+            $('#sidebar').find('li').find('a').removeClass('active');
+            $('#profile_link').addClass('active');
+        } else if(skill < 100 && skill > -100) {
+            $('#sidebar').find('li').find('a').removeClass('active');
+            $('#skill_link').addClass('active');
+        } else if(contact < 100 && contact > -100) {
+            $('#sidebar').find('li').find('a').removeClass('active');
+            $('#contact_link').addClass('active');
+        }
+
+    });
+
+    //workモーダル制御
+
+    //クローズ
+    $('.modal').on('click', function() {
+        $('.modal').fadeOut();
+    });
+
+    // work1
+    $('.work1').on('click', function() {
+        $('.work1-modal').fadeIn();
+    });
+    $('#work1-link').on('click', function() {	
+        const url = 'https://www.oshiire.co.jp/';
+        window.open(url, '_blank');
+        return false;
+    });
+
+    // work2
+    $('.work2').on('click', function() {
+        $('.work2-modal').fadeIn();
+    });
+    $('#work2-link').on('click', function() {	
+        const url = 'https://logiselect.oshiire.co.jp/';
+        window.open(url, '_blank');
+        return false;
+    });
+
+    //スクロールで要素の表示制御
+    $('#top-contents').scroll(function () {
+        $('.fade').removeClass('view');
+        $('.fade').each(function () {
+            const targetElement = $(this).offset().top;
+            const scroll = $(window).scrollTop();
+            const windowHeight = $(window).height();
+            if (scroll > targetElement - windowHeight) {
+                $(this).addClass('view');
+            }
+        });
+    });
+});

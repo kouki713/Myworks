55e5079eeed316bbccd48d7dc0b81c5e23c3c5a2
k.sawada <63055099+koukisawada1@users.noreply.github.com>
2022-03-13 23:31:35 +0900

    profileページ作成

--- all.js
+++ all.js
@@ -85,4 +85,11 @@ $(function() {
             }
         });
     });
+
+    //profileアコーディオン
+    $('.s_01 .accordion_one .accordion_header').click(function(){
+        //クリックされた.accordion_oneの中の.accordion_headerに隣接する.accordion_innerが開いたり閉じたりする。
+        $(this).next('.accordion_inner').slideToggle();
+        $(this).toggleClass("open");
+    });
 });
